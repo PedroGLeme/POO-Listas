4e2a06d013ff9e70eaba7629bb1f0b5bb5d2a35c
PedroGLeme <135152187+PedroGLeme@users.noreply.github.com>
2024-09-05 10:13:30 -0300

    ex6

--- lista01/ex6.cpp
+++ lista01/ex6.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// funcao que retorna a soma 2 valores double
 int soma(double a, double b){
     int aux = a + b;
     return aux;
@@ -12,8 +13,8 @@ int main()
 {
     double v1, v2;
     int res;
-    cin >> v1 >> v2;
-    res=soma(v1, v2);
-    cout << res;
+    cin >> v1 >> v2; // input de 2 valores double
+    res=soma(v1, v2); // chama a funcao que soma os 2 valores
+    cout << res; // print do resultado
     return 0;
 }
