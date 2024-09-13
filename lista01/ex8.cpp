9634056df497efe56904a1e55ab917ba1c573fee
PedroGLeme <135152187+PedroGLeme@users.noreply.github.com>
2024-09-13 09:23:44 -0300

    ex8

--- lista01/ex8.cpp
+++ lista01/ex8.cpp
@@ -7,24 +7,28 @@
 using namespace std;
 
 string conteudo(std::string nome){
-    //char* linha = new char[1000001];
-    ifstream arquivo(nome);
-    string acumulador_linha, linha;
+
+    ifstream arquivo(nome); // abertura de arquivo pelo nome dado pelo operador
+    string acumulador_linha, linha; // crição de duas variaveis string, onde uma faz a leitgura ate o \n e a outra sera a string retornada com todo conteudo do arquivo
         // Lê o arquivo linha por linha
         while (getline(arquivo, linha)) {
-            acumulador_linha += linha;
+            acumulador_linha += linha + '\n'; // acumula as linhas e adiciona \n
         }
-    arquivo.close();
+    arquivo.close(); // fecha arquivo
     return acumulador_linha;
 }
 
 int main()
 {
+    // pega o nome do arquivo ditado pelo operador
     string nome_arquivo;
-    string conteudo_arq;
     cin >> nome_arquivo;
+    
+    // pega o conteudo do arquivo e faz chamada da funcao
+    string conteudo_arq;
     conteudo_arq = conteudo(nome_arquivo);
 
+    // print do conteudo 
     cout << conteudo_arq << endl;
 
     return 0;
