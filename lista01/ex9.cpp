78e3daf6c27bc3fe467f3d718bb7493d3baf6aa3
PedroGLeme <135152187+PedroGLeme@users.noreply.github.com>
2024-09-13 20:57:36 -0300

    ex9

--- lista01/ex9.cpp
+++ lista01/ex9.cpp
@@ -10,12 +10,12 @@ int main() {
     // Leitura dos valores
     cin >> x1 >> y1 >> x2 >> y2;
 
-    // Cálculo da distância euclidiana
+    // Cálculo da distância
     double dist;
     dist = sqrt((x1-x2)*(x1-x2) + (y1-y2)*(y1-y2));
 
     // Exibe a distância com 4 casas decimais
-    cout << setprecision(5) << dist << endl;
+    cout << fixed << setprecision(4) << dist << endl;
 
     return 0;
 }
