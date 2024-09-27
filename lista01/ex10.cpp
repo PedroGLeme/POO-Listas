75b9de47f8f3f1e3307e65b8642ea34c197cb507
PedroGLeme <135152187+PedroGLeme@users.noreply.github.com>
2024-09-27 03:30:49 -0300

    ex10

--- lista01/ex10.cpp
+++ lista01/ex10.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <iomanip> // Para controlar a precisão da saída
+#include <iomanip> // Para controlar a precis�o da sa�da
 
 using namespace std;
 
@@ -7,30 +7,34 @@ int main() {
     // Valor das notas
     int notas[] = {20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 25, 10, 5, 1};
 
-    double valor, Pago;
-
+    int valor, Pago;
+    double d_valor, d_pago;
     // Entrada dos valor do produto e do dinheiro pago
-    cin >> valor >> Pago;
+    cin >> d_valor;                    // recebe o valor da conta
+    valor = (int)(100 * d_valor);
+
+    cin >> d_pago;                    // recebe o valor pago
+    Pago = (int)(100*d_pago);
 
     // Calcula o troco
-    double troco = Pago - valor;
+    double troco = (d_pago - d_valor);
 
-    // Converte o troco em centavos para facilitar o cálculo
-    int trocoCentavos = (troco * 100);
+    // Converte o troco em centavos para facilitar o c�lculo
+    int trocoCentavos = Pago - valor;
 
     cout << fixed << setprecision(2) << "Troco: R$ " << troco << endl;
 
     int contador=0;
-    // Calcula o número de notas
+    // Calcula o n�mero de notas
     for (int i = 0; i < 13; i++) {
         while(1){
-            if((trocoCentavos-notas[i])<0){ // caso subtração do troco menor que zero, vai para proxima menor nota
+            if((trocoCentavos-notas[i])<0){ // caso subtra��o do troco menor que zero, vai para proxima menor nota
                 break;
             }
-            trocoCentavos = trocoCentavos - notas[i]; // subtração que sinaliza, possibilidade de troco com tal nota
+            trocoCentavos = trocoCentavos - notas[i]; // subtra��o que sinaliza, possibilidade de troco com tal nota
             contador++; // numero de notas necessarias para o troco
         }
-
+        fflush(stdin);
         cout << contador << " de R$ " << fixed << setprecision(2)<< float(notas[i])/100 << endl; // impressao dos resultados
         contador = 0;
     }
