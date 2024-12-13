2d07e506119e01772c6f46dfd7227ddbf7bebdbf
PedroGLeme <135152187+PedroGLeme@users.noreply.github.com>
2024-12-12 21:14:28 -0300

    ex3

---
+++ lista04/ex3.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+#include <algorithm>
+
+using namespace std;
+
+bool eh_stack(vector<int> in, vector<int> out){
+    if(in.size() != out.size()){ // caso tenham tamanhos diferentes, ja retorna falso
+        return false;
+    } 
+    int k = in.size()-1;
+    int j=0;
+    for(j = 0; j<out.size(); j++){
+        if(k>=0){
+            return true; //chegou no final
+        }
+        if(in[k] != out[j]){
+            return false;
+        } 
+        k--;
+    }
+
+    return true;
+}
+
+bool eh_queue(vector<int> in, vector<int> out){
+    if(in.size()!=out.size()){
+        return false;
+    }
+
+    for(int k=0; k< out.size(); k++){
+        if(in[k]!=out[k]){
+            return false;
+        }
+    }
+    return true;
+}
+
+bool eh_prioridade(vector<int> in, vector<int> out){
+    if(in.size()!=out.size()){
+        return false;
+    }
+
+    sort(in.begin(), in.end()); // caso a entrada esteja em ordem crescente, coincide com a ordem de prioridade
+
+    int k = in.size()-1;
+    int j=0;
+    for(j = 0; j<out.size(); j++){
+        if(k>=0){
+            return true; //chegou no final
+        }
+        if(in[k] != out[j]){
+            return false;
+        } 
+        k--;
+    }
+
+    return true;
+}
+
+int main(){
+    char cmd[5]; int numero;
+    vector<int> vet_add, vet_pop;
+
+    while(scanf("%s %d", cmd, &numero)!= -1){
+        if(strcmp(cmd, "add") == 0){vet_add.push_back(numero);}
+        else if(strcmp(cmd, "pop") == 0){vet_pop.push_back(numero);}
+        else exit(-1);
+    }
+
+    if(eh_stack(vet_add, vet_pop)){
+        if(eh_queue(vet_add, vet_pop)|| eh_prioridade(vet_add, vet_pop)){printf("both\n");}
+        else{printf("stack\n");}
+    }
+    else if(eh_queue(vet_add, vet_pop)){
+        if(eh_stack(vet_add, vet_pop)|| eh_prioridade(vet_add, vet_pop)){printf("both\n");}
+        else{printf("queue\n");}
+    }
+    else if(eh_prioridade(vet_add, vet_pop)){
+        if(eh_stack(vet_add, vet_pop)|| eh_queue(vet_add, vet_pop)){printf("both\n");}
+        else{printf("priority\n");}
+    }
+    
+}       
