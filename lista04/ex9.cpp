5f3a0347c872e99bec4633f18067d814501a4db8
PedroGLeme <135152187+PedroGLeme@users.noreply.github.com>
2024-12-14 16:20:36 -0300

    ex9 com comentarios

--- lista04/ex9.cpp
+++ lista04/ex9.cpp
@@ -6,19 +6,19 @@ using namespace std;
 
 vector<double> notas(const string& s){
     vector<double> notas_ret;
-    int i = 0;
-    while (i < s.size()){
-        while (i < s.size() && s[i] != ','){ i++;}// acha a primeira virgula
-        i++; // pula a virgula
-        // le o numero
+    int i= 0;
+    while (i< s.size()){
+        while (i< s.size() && s[i]!= ','){ i++;}// acha a primeira virgula
+        i++; //pula a virgula
+        //le o numero
         string numero;
         while (i< s.size() && s[i]!= ','){ // ate chegar no fim da string dada ou ate a virgula
             numero += s[i]; // contatena o numero
             i++;
         }
         // transforma streing em double
-        if (!(numero.empty())){ notas_ret.push_back(stod(numero)); }
-        i++; // pula a virgula do numero
+        if( (!(numero.empty()))){notas_ret.push_back(stod(numero)); }
+        i++; //pula a virgula do numero
     }
 
     return notas_ret;
